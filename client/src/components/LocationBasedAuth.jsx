4c80827d247bf6e6e173adab2c1393c401416aa9
aniruddhabharambe1905@gmail.com <your-email@example.com>
2025-03-24 22:42:28 +0530

    Initial commit - YouTube Clone

--- client/src/components/LocationBasedAuth.jsx
+++ client/src/components/LocationBasedAuth.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import { isSouthIndianLocation } from '../utils/themeUtils';
 
 const LocationBasedAuth = () => {
